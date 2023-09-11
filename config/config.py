c03c98922a0a3a962d5ac7ce9519ebdfb0615ac1
Tobias Seidel <tgseidel@amazon.de>
2023-09-11 09:39:55 +0200

    Update context

--- config/config.py
+++ config/config.py
@@ -2,7 +2,9 @@ from collections import namedtuple
 
 def get_config(app):
     context = app.node.try_get_context("context")
+    print(context)
     env = app.node.try_get_context("env")
+    print(env)
 
     Config = namedtuple("Config", "memory_size")
     config = Config(context[env]["memory_size"])
