0fda629ef4435fad9c3b5124ae5dbbf50f13d3cd
Tobias Seidel <tgseidel@amazon.de>
2023-09-11 09:34:33 +0200

    Fix tests

--- unittests/test_lambda_function.py
+++ unittests/test_lambda_function.py
@@ -1,13 +1,15 @@
 from aws_cdk import core
 from pipeline.lambda_stack import LambdaStack
+from config.config import get_config
 
 def test_lambda_handler():
 
     # GIVEN
     app = core.App()
+    config = get_config(app)
 
     # WHEN
-    LambdaStack(app, 'Stack', 'UnitTestTag')
+    LambdaStack(app, 'Stack', 'UnitTestTag', config)
 
     # THEN
     template = app.synth().get_stack_by_name('Stack').template
